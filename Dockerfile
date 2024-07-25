e0f5539d7515f87318d29a42f84c6047688fd72d
Victor Canto <iamvictorcanto@gmail.com>
2024-07-25 17:47:05 -0300

    feat: add project

---
+++ Dockerfile
@@ -0,0 +1,13 @@
+FROM node:18 AS node_stage
+WORKDIR /app
+COPY package*.json ./
+RUN npm install
+COPY . .
+RUN npm run build
+
+FROM nginx:alpine
+RUN apk add --no-cache nodejs npm
+COPY nginx.conf /etc/nginx/nginx.conf
+COPY --from=node_stage /app/dist /app/dist
+EXPOSE 80
+CMD ["sh", "-c", "node /app/dist/ssr-nginx-poc/server/server.mjs & nginx -g 'daemon off;'"]
--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,16 +14,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'angular-ssr-nginx' title`, () => {
+  it(`should have the 'ssr-nginx-poc' title`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('angular-ssr-nginx');
+    expect(app.title).toEqual('ssr-nginx-poc');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, angular-ssr-nginx');
+    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, ssr-nginx-poc');
   });
 });
